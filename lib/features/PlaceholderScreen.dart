80f4f2ff3a847bbe14c3bd73895e68e74dac6bac
Lucas <comosellacc@gmail.com>
2020-03-17 15:40:42 +0100

    feat(UserProfile): initializing store connector to get uid

--- lib/features/PlaceholderScreen.dart
+++ lib/features/PlaceholderScreen.dart
@@ -1,16 +1,33 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:myplaces/features/viewmodels/AuthViewModel.dart';
+import 'package:myplaces/features/viewmodels/ViewModel.dart';
+import 'package:myplaces/redux/AppState.dart';
+import 'package:redux/redux.dart';
 import './menu/MainMenu.dart';
 
 class PlaceholderScreen extends StatelessWidget {
 
-  Widget _getBody() => const Center(
+  Widget _getBody(ViewModel vm) => const Center(
     child: Text('Placeholder Screen'),
+    
   );
 
   @override
   Widget build(BuildContext context) {
-    return MainMenu(_getBody());
+    return Scaffold(
+      body: Builder(
+        builder: (BuildContext context) {
+          return StoreConnector<AppState, ViewModel>(
+            distinct: true,
+            converter: (Store<AppState> store) => AuthViewModel.fromStore(store),
+            builder: (BuildContext, ViewModel vm) {
+              return MainMenu(_getBody(vm));
+            },
+          );
+        }
+      ),
+    );
   }
-
 }
