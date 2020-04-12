f7ad24930ec313c0762c757eb85728c19f4f0fd3
slim <slim.helaoui@epitech.eu>
2020-04-12 22:42:41 +0200

    limit titel max 15

--- lib/features/newPlace/NewPlace.dart
+++ lib/features/newPlace/NewPlace.dart
@@ -165,7 +165,7 @@ class NewPlaceState extends State<NewPlace>{
         children: <Widget>[
           Padding(
               child: TextFormField(
-                maxLength : 29,
+                maxLength : 15,
                 style: const TextStyle(color: Colors.white),
                 maxLines: 1,
                 obscureText: false,
